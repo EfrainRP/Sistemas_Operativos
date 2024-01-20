f15d10e1a616719f23557ca316995f4ca7f621d3
EfrainRP <efrain.robles5009@alumnos.udg.mx>
2024-01-20 12:24:51 -0600

    Interface-Practice 1

---
+++ Practica1_UI_Java/src/Practica1.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Practica1 extends JFrame implements ActionListener {
+    private JLabel IbNombre;
+    private JTextField txNombre;
+    private JButton btVer;
+
+    public Practica1(){
+        this.setBounds(0,0,350,120);
+        this.setLayout(null);
+
+        IbNombre =new JLabel(); //Creamos objeto
+        IbNombre.setBounds(10,10,150,30);
+        IbNombre.setName("IbNombre");
+        IbNombre.setText("Ingrese nombre: ");
+        IbNombre.setVisible(true);
+        this.add(IbNombre);
+
+        txNombre = new JTextField();
+        txNombre.setBounds(115,10,100,30);
+        txNombre.setName("txNombre");
+        txNombre.setVisible(true);
+        txNombre.setText(" ");
+        this.add(txNombre);
+
+        btVer = new JButton();
+        btVer.setBounds(215,10,70,30);
+        btVer.setName("btVer");
+        btVer.setText("Ver");
+        btVer.setVisible(true);
+        btVer.addActionListener(this);
+        this.add(btVer);
+
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Cerrado automatico
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == btVer){
+            JOptionPane.showMessageDialog(this,"Hola: "+ txNombre.getText());
+        }
+    }
+    public static void main(String[] args) {
+        Practica1 ob = new Practica1();
+        ob.setVisible(true);
+    }
+}
