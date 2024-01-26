45feb3c1adce77f48bd3fd9da14f0670e8673cea
EfrainRP <efrain.robles5009@alumnos.udg.mx>
2024-01-26 16:53:37 -0600

    Finished

--- Practica2_Python/UI_tkinter.py
+++ Practica2_Python/UI_tkinter.py
@@ -1,103 +1,182 @@
 import tkinter as tk
 from tkinter import END,messagebox
 import math as m
+import re
 
 root=tk.Tk()
 root.title("Calculadora")
-root.config(width=300,height=500)
+root.config(width=330,height=500)
 aux=0
 signo=""
 
 def operador(x):
+    clear()
     global signo,aux
     signo=x
-    aux=float(txtDisplay.get())
+    aux=txtDisplay.get()
     txtDisplay.delete(0,END)
 
-    if signo=="n!":
-        resultado = 1
-        for i in range(2,int(aux)+1):
-            resultado = resultado * i
-        txtDisplay.insert(0,str(resultado))
-    elif signo=="sqrt":
-        resultado=m.sqrt(aux)
-        txtDisplay.insert(0,str(resultado))
-    elif signo=="DecBin":
-        resultado = bin(int(aux))
-        resultado = resultado.removeprefix('0b')
-        # resultado=format(int(aux),'o')
-        txtDisplay.insert(0,str(resultado))
-    elif signo=="DecOct":
-        resultado = oct(int(aux))
-        resultado = resultado.removeprefix('0o')
-        # resultado=format(int(aux),'o')
-        txtDisplay.insert(0,str(resultado))
-    elif signo=="DecHex":
-        resultado = hex(int(aux))
-        resultado = resultado.removeprefix('0x')
-        # resultado=format(int(aux),'o')
-        txtDisplay.insert(0,str(resultado))
-    elif signo=="BinDec": ######################
-        resultado = bin(int(aux))
-        resultado = resultado.removeprefix('0b')
-        # resultado = int(resultado,2)
-        #resultado = int(str(resultado),2)
-        print(int(str(resultado),2))
-        # resultado=format(int(aux),'o')
-        txtDisplay.insert(0,str(int(resultado,2)))
-    elif signo=="OctDec":
-        resultado = hex(int(aux))
-        resultado = resultado.removeprefix('0x')
-        # resultado=format(int(aux),'o')
-        txtDisplay.insert(0,str(resultado))
-    elif signo=="HexDec":
-        resultado = hex(int(aux))
-        resultado = resultado.removeprefix('0x')
-        # resultado=format(int(aux),'o')
-        txtDisplay.insert(0,str(resultado))    
+    regex = re.compile(r'[+-]?\d*\.?\d+') # Acepta numeros + y - asi como decimales y enteros
+    if regex.fullmatch(aux):
+        aux=float(aux)
+        if signo=="n!":
+            resultado = 1
+            for i in range(2,int(aux)+1):
+                resultado = resultado * i
+            txtDisplay.insert(0,str(resultado))
+            txtOperators.insert(0,f'{aux}! = {resultado}')
+
+        elif signo=="sqrt":
+            resultado=m.sqrt(aux)
+            txtDisplay.insert(0,str(resultado))  
+            txtOperators.insert(0,f'sqrt({aux}) = {resultado}')
+    else:
+        messagebox.showinfo(message="Caracteres no aceptados", title="Error de Entrada")
 
 def operacion():
     resultado=0
     global signo,aux
-    if signo=="+":
-        resultado=aux+float(txtDisplay.get())
-    elif signo=="-":
-        resultado=aux-float(txtDisplay.get())
-    elif signo=="*":
-        resultado=aux*float(txtDisplay.get())
-    elif signo=="/":
-        resultado=aux/float(txtDisplay.get())
-    elif signo=="^":
-        resultado=aux**float(txtDisplay.get())
-    elif signo=="%":
-        resultado=aux%float(txtDisplay.get())
-    txtDisplay.delete(0,END)
-    txtDisplay.insert(0,str(resultado))
+
+    regex = re.compile(r'[+-]?\d*\.?\d+') # Acepta numeros + y - asi como decimales y enteros
+    if regex.fullmatch(txtDisplay.get()) :
+
+        if signo=="+":
+            resultado=aux+float(txtDisplay.get())
+            txtOperators.insert(0,f'{aux} + {float(txtDisplay.get())} = {resultado}')
+        elif signo=="-":
+            resultado=aux-float(txtDisplay.get())
+            txtOperators.insert(0,f'{aux} - {float(txtDisplay.get())} = {resultado}')
+        elif signo=="*":
+            resultado=aux*float(txtDisplay.get())
+            txtOperators.insert(0,f'{aux} x {float(txtDisplay.get())} = {resultado}')
+        elif signo=="/":
+            if txtDisplay.get() == "0":
+                messagebox.showinfo(message="Division entre 0, no valido", title="Error de Calculo")
+            else:
+                resultado=aux/float(txtDisplay.get())
+                txtOperators.insert(0,f'{aux} / {float(txtDisplay.get())} = {resultado}')
+        elif signo=="^":
+            resultado=aux**float(txtDisplay.get())
+            txtOperators.insert(0,f'{aux} ^ {float(txtDisplay.get())} = {resultado}')
+        elif signo=="%":
+            if txtDisplay.get() == "0":
+                messagebox.showinfo(message="Division entre 0, no valido", title="Error de Calculo")
+            else:
+                resultado=aux%float(txtDisplay.get())
+                txtOperators.insert(0,f'{aux} % {float(txtDisplay.get())} = {resultado}')
+        txtDisplay.delete(0,END)
+        txtDisplay.insert(0,str(resultado))
+
+def conversion(x):
+    clear()
+
+    global signo,aux
+    signo=x
+    aux=txtDisplay.get()
+    txtDisplay.delete(0,END) #############
+
+    regex = re.compile(r'^[0-9A-Fa-f]+$') #Acepta numeros del 0 al 9 asi como de la A a la F, y minusculas de esas letras
+    if regex.fullmatch(aux):
+        if signo=="DecBin":
+            try:
+                resultado = bin(int(aux))
+                resultado = resultado.removeprefix('0b')
+                BinOut.insert(0,str(resultado))
+            except:
+                print('Tipo de Valor Incorrecto')
+
+        elif signo=="DecOct":
+            try:
+                resultado = oct(int(aux))
+                resultado = resultado.removeprefix('0o')
+                OctOut.insert(0,str(resultado))
+            except:
+                print('Tipo de Valor Incorrecto')
+
+        elif signo=="DecHex":
+            try:
+                resultado = hex(int(aux))
+                resultado = resultado.removeprefix('0x')
+                HexOut.insert(0,str(resultado))
+            except:
+                print('Tipo de Valor Incorrecto')
+
+        elif signo=="BinDec":
+            try:
+                resultado = int(aux)
+                # print(aux)
+                # print(int(str(int(aux)),2))
+                resultado = int(str(resultado),2)
+                DecOut.insert(0,str(resultado))
+            except:
+                print('Tipo de Valor Incorrecto')
+
+        elif signo=="OctDec":
+            try:
+                resultado = int(aux)
+                resultado = int(str(resultado),8)
+                DecOut.insert(0,str(resultado))
+            except:
+                print('Tipo de Valor Incorrecto')
+
+        elif signo=="HexDec":
+            try:
+                # resultado = aux
+                resultado = int(str(aux),16)  
+                HexOut.insert(0,str(resultado))
+            except:
+                print('Tipo de Valor Incorrecto')
+        
+        txtDisplay.insert(0,str(resultado)) ##############
+    else:
+        messagebox.showinfo(message="Decimales no aceptados o caracteres no aceptados", title="Error de Conversión")
+
+def clear():
+    DecOut.delete(0,END)
+    OctOut.delete(0,END)
+    HexOut.delete(0,END)
+    BinOut.delete(0,END)
+    txtOperators.delete(0,END)
 
 txtDisplay=tk.Entry(root)
-txtDisplay.place(x=10,y=20)
-tk.Label(root,text="DEC:").place(x=10,y=40)
-tk.Label(root,text="OCT:").place(x=10,y=60)
+txtDisplay.place(x=45,y=20)
 
-tk.Label(root,text="HEX:").place(x=10,y=80)
-tk.Label(root,text="BIN:").place(x=10,y=100)
+tk.Label(root,text="Ope:").place(x=10,y=50)
+txtOperators=tk.Entry(root)
+txtOperators.place(x=45,y=50)
 
-btnDecBin = tk.Button(root, text="DecBin", command=lambda: operador("DecBin"))
+tk.Label(root,text="DEC:").place(x=10,y=80)
+DecOut = tk.Entry(root)
+DecOut.place(x=45,y=80)
+
+tk.Label(root,text="OCT:").place(x=10,y=110)
+OctOut = tk.Entry(root)
+OctOut.place(x=45,y=110)
+
+tk.Label(root,text="HEX:").place(x=10,y=140)
+HexOut = tk.Entry(root)
+HexOut.place(x=45,y=140)
+
+tk.Label(root,text="BIN:").place(x=10,y=170)
+BinOut = tk.Entry(root)
+BinOut.place(x=45,y=170)
+
+btnDecBin = tk.Button(root, text="DecBin", command=lambda: conversion("DecBin"))
 btnDecBin.place(x=200,y=20)
 
-btnDecOct = tk.Button(root, text="DecOct", command=lambda: operador("DecOct"))
+btnDecOct = tk.Button(root, text="DecOct", command=lambda: conversion("DecOct"))
 btnDecOct.place(x=200,y=60)
 
-btnDecHex = tk.Button(root, text="DecHex", command=lambda: operador("DecHex"))
+btnDecHex = tk.Button(root, text="DecHex", command=lambda: conversion("DecHex"))
 btnDecHex.place(x=200,y=100)
 
-btnBinDec = tk.Button(root, text="BinDec", command=lambda: operador("BinDec"))
+btnBinDec = tk.Button(root, text="BinDec", command=lambda: conversion("BinDec"))
 btnBinDec.place(x=200,y=140)
 
-btnOctDec = tk.Button(root, text="OctDec", command=lambda: operador("OctDec"))
+btnOctDec = tk.Button(root, text="OctDec", command=lambda: conversion("OctDec"))
 btnOctDec.place(x=200,y=180)
 
-btnHexDec = tk.Button(root, text="HexDec", command=lambda: operador("HexDec"))
+btnHexDec = tk.Button(root, text="HexDec", command=lambda: conversion("HexDec"))
 btnHexDec.place(x=200,y=220)
 
 btnExp = tk.Button(root, text="^", command=lambda: operador("^"))
