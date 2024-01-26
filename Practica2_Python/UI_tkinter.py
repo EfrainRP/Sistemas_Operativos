f2ba78694e609d4576f772d0d2bdc2d4cb061336
EfrainRP <efrain.robles5009@alumnos.udg.mx>
2024-01-26 01:14:40 -0600

    advance calculator

--- Practica2_Python/UI_tkinter.py
+++ Practica2_Python/UI_tkinter.py
@@ -14,6 +14,48 @@ def operador(x):
     aux=float(txtDisplay.get())
     txtDisplay.delete(0,END)
 
+    if signo=="n!":
+        resultado = 1
+        for i in range(2,int(aux)+1):
+            resultado = resultado * i
+        txtDisplay.insert(0,str(resultado))
+    elif signo=="sqrt":
+        resultado=m.sqrt(aux)
+        txtDisplay.insert(0,str(resultado))
+    elif signo=="DecBin":
+        resultado = bin(int(aux))
+        resultado = resultado.removeprefix('0b')
+        # resultado=format(int(aux),'o')
+        txtDisplay.insert(0,str(resultado))
+    elif signo=="DecOct":
+        resultado = oct(int(aux))
+        resultado = resultado.removeprefix('0o')
+        # resultado=format(int(aux),'o')
+        txtDisplay.insert(0,str(resultado))
+    elif signo=="DecHex":
+        resultado = hex(int(aux))
+        resultado = resultado.removeprefix('0x')
+        # resultado=format(int(aux),'o')
+        txtDisplay.insert(0,str(resultado))
+    elif signo=="BinDec": ######################
+        resultado = bin(int(aux))
+        resultado = resultado.removeprefix('0b')
+        # resultado = int(resultado,2)
+        #resultado = int(str(resultado),2)
+        print(int(str(resultado),2))
+        # resultado=format(int(aux),'o')
+        txtDisplay.insert(0,str(int(resultado,2)))
+    elif signo=="OctDec":
+        resultado = hex(int(aux))
+        resultado = resultado.removeprefix('0x')
+        # resultado=format(int(aux),'o')
+        txtDisplay.insert(0,str(resultado))
+    elif signo=="HexDec":
+        resultado = hex(int(aux))
+        resultado = resultado.removeprefix('0x')
+        # resultado=format(int(aux),'o')
+        txtDisplay.insert(0,str(resultado))    
+
 def operacion():
     resultado=0
     global signo,aux
@@ -29,15 +71,6 @@ def operacion():
         resultado=aux**float(txtDisplay.get())
     elif signo=="%":
         resultado=aux%float(txtDisplay.get())
-    elif signo=="n!":
-        start = 1
-        for i in range(1,txtDisplay.get()): ## TO DO
-            start = start * i
-        resultado = start
-    elif signo=="sqrt":
-        resultado=m.sqrt(aux)
-    
-
     txtDisplay.delete(0,END)
     txtDisplay.insert(0,str(resultado))
 
@@ -48,6 +81,24 @@ tk.Label(root,text="OCT:").place(x=10,y=60)
 
 tk.Label(root,text="HEX:").place(x=10,y=80)
 tk.Label(root,text="BIN:").place(x=10,y=100)
+
+btnDecBin = tk.Button(root, text="DecBin", command=lambda: operador("DecBin"))
+btnDecBin.place(x=200,y=20)
+
+btnDecOct = tk.Button(root, text="DecOct", command=lambda: operador("DecOct"))
+btnDecOct.place(x=200,y=60)
+
+btnDecHex = tk.Button(root, text="DecHex", command=lambda: operador("DecHex"))
+btnDecHex.place(x=200,y=100)
+
+btnBinDec = tk.Button(root, text="BinDec", command=lambda: operador("BinDec"))
+btnBinDec.place(x=200,y=140)
+
+btnOctDec = tk.Button(root, text="OctDec", command=lambda: operador("OctDec"))
+btnOctDec.place(x=200,y=180)
+
+btnHexDec = tk.Button(root, text="HexDec", command=lambda: operador("HexDec"))
+btnHexDec.place(x=200,y=220)
 
 btnExp = tk.Button(root, text="^", command=lambda: operador("^"))
 btnExp.place(x=280,y=20)
